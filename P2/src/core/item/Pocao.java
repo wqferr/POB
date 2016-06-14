dc9dd30f07aa5be9bf0e58edb51e17dc5e6f4e37
wqferr <wqferr@gmail.com>
2016-06-14 11:20:01 -0300

    Apagar projeto antigo

--- P2/src/core/item/Pocao.java
+++
@@ -1,9 +0,0 @@
-package core.item;
-
-public abstract class Pocao extends ItemUsavel {
-
-	public Pocao(String nome) {
-		super(nome);
-	}
-
-}
--- P2/src/core/personagem/Profissao.java
+++
@@ -1,8 +0,0 @@
-package core.personagem;
-
-public enum Profissao {
-	GUERREIRO,
-	MAGO,
-	SACERDOTE,
-	ARQUEIRO;
-}
