01534e097800b83fdfbc18b04e5f073f39378c6a
wqferr <wqferr@gmail.com>
2016-06-14 23:20:00 -0300

    Remover método isEquipavel de ItemUsavel

--- RP2G/src/core/item/usavel/ItemUsavel.java
+++ RP2G/src/core/item/usavel/ItemUsavel.java
@@ -2,17 +2,12 @@ package core.item.usavel;
 
 import core.item.Item;
 import core.personagem.Personagem;
-import core.personagem.Profissao;
 import exception.NomeRepetidoException;
 
 public abstract class ItemUsavel extends Item {
 	
 	public ItemUsavel(String nome) throws NomeRepetidoException {
 		super(nome);
-	}
-	
-	public boolean isEquipavel(Profissao p) {
-		return false;
 	}
 	
 	public abstract void usar(Personagem p);
