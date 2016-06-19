fdfb4ba0c19bdd550a57849147890c9d01227010
wqferr <wqferr@gmail.com>
2016-06-19 16:33:33 -0300

    Implementar métodos de controle do jogo

--- RP2G/src/core/Jogo.java
+++ RP2G/src/core/Jogo.java
@@ -9,6 +9,7 @@ import core.item.Item;
 import core.mapa.Mapa;
 import core.mapa.Posicao;
 import core.personagem.Personagem;
+import core.personagem.Personagem.Stat;
 
 public class Jogo {
 	
@@ -36,19 +37,26 @@ public class Jogo {
 	}
 	
 	public boolean mover(Posicao nova) {
+		if (this.mapa.alcancavel(this.pAtual.getPosicao(), nova, this.pAtual.getStat(Stat.VEL))) {
+			this.mapa.mover(this.pAtual.getPosicao(), nova);
+			return true;
+		}
 		return false;
 	}
 	
 	public boolean atacar(Posicao alvo) {
-		return false;
+		if (!this.mapa.isOcupado(alvo) || this.pAtual.getArma() == null)
+			return false;
+		
+		return alvo.distancia(this.pAtual.getPosicao()) > this.pAtual.getArma().getAlcance();
 	}
 	
 	public boolean usar(Item item) {
-		return false;
+		return this.pAtual.usar(item);
 	}
 	
 	public boolean usar(String item) {
-		return false;
+		return this.pAtual.usar(item);
 	}
 
 }
