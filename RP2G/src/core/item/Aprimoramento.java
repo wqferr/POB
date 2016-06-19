556254fcb5da50526342a9a0a328a5a121980295
wqferr <wqferr@gmail.com>
2016-06-19 16:21:55 -0300

    Adicionar stat VEL

--- RP2G/src/core/item/Aprimoramento.java
+++ RP2G/src/core/item/Aprimoramento.java
@@ -9,12 +9,14 @@ public class Aprimoramento extends Item {
 	private final int bonusFor;
 	private final int bonusDex;
 	private final int bonusInt;
+	private final int bonusVel;
 	
-	public Aprimoramento(String nome, int bf, int bd, int bi) throws NomeRepetidoException {
+	public Aprimoramento(String nome, int bf, int bd, int bi, int bv) throws NomeRepetidoException {
 		super(nome);
 		this.bonusFor = bf;
 		this.bonusDex = bd;
 		this.bonusInt = bi;
+		this.bonusVel = bv;
 	}
 
 	@Override
@@ -22,6 +24,7 @@ public class Aprimoramento extends Item {
 		p.setStat(Stat.FOR, p.getStat(Stat.FOR) + this.bonusFor);
 		p.setStat(Stat.DEX, p.getStat(Stat.DEX) + this.bonusDex);
 		p.setStat(Stat.INT, p.getStat(Stat.INT) + this.bonusInt);
+		p.setStat(Stat.VEL, p.getStat(Stat.VEL) + this.bonusVel);
 		return true;
 	}
 
