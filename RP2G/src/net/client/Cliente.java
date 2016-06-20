1e8b08f6ad1b77d9d31ba890410c3648f510d1d8
wqferr <wqferr@gmail.com>
2016-06-19 22:00:08 -0300

    Implementar inicio da comunicação do lado do cliente

--- RP2G/src/net/client/Cliente.java
+++ RP2G/src/net/client/Cliente.java
@@ -1,21 +1,57 @@
 package net.client;
 
 import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
 import java.net.InetAddress;
 import java.net.Socket;
 
+import exception.DesyncException;
+import net.Mensagem;
+import net.Mensagem.Evento;
 import net.server.Servidor;
 
 public class Cliente {
 	
 	private Socket conexao;
-
-	public void conectar(InetAddress ip, int porta) throws IOException {
-		this.conexao = new Socket(ip, porta);
-	}
+	private boolean vez;
+	private ObjectInputStream in;
+	private ObjectOutputStream out;
 	
 	public void conectar(InetAddress ip) throws IOException {
 		this.conectar(ip, Servidor.PORTA_PADRAO);
+	}
+
+	public void conectar(InetAddress ip, int porta) throws IOException {
+		this.conexao = new Socket(ip, porta);
+		this.in = new ObjectInputStream(this.conexao.getInputStream());
+		this.out = new ObjectOutputStream(this.conexao.getOutputStream());
+		Mensagem msg = this.receber();
+		if (msg.getEvento() != Evento.INICIO_CONEXAO)
+			this.notificarDessincronia();
+		
+		this.vez = "1".equals(msg.getMsg());
+	}
+	
+	public Mensagem receber() throws IOException {
+		try {
+			return (Mensagem) this.in.readObject();
+		} catch (ClassNotFoundException e) {
+			return null;
+		}
+	}
+	
+	public void enviar(Mensagem m) throws IOException {
+		this.out.writeObject(m);
+	}
+	
+	public void notificar(Evento e) throws IOException {
+		this.enviar(new Mensagem(e));
+	}
+	
+	public void notificarDessincronia() throws DesyncException, IOException {
+		this.notificar(Evento.DESSINCRONIA);
+		throw new DesyncException();
 	}
 
 }
