379e3be369cc6155484a9e82a32c162432226eb4
wqferr <wqferr@gmail.com>
2016-06-21 15:48:00 -0300

    Implementar ciclo básico de comandos no cliente

--- RP2G/src/test/TestClient.java
+++ RP2G/src/test/TestClient.java
@@ -5,29 +5,62 @@ import java.net.InetAddress;
 import java.util.Scanner;
 
 import net.client.Cliente;
+import net.client.Controlador;
 import net.client.Ordem;
 import net.client.Ordem.Comando;
 import net.server.Servidor;
 import core.Jogo;
+import core.mapa.Posicao;
 
 public class TestClient {
+	
+	private static boolean andou = false;
+	private static boolean atacou = false;
 
 	public static void main(String[] args) {
-		boolean andou = false;
-		boolean atacou = false;
 		try (final Scanner in = new Scanner(System.in)) {
             in.useDelimiter("[^\\s]");
-            Cliente c = new Cliente((Jogo j) -> {
-                if (!in.hasNext())
-                	return new Ordem(Comando.ENCERRAR);
-
-                switch (in.next()) {
-                case "m":
-                	break;
+            Controlador cont = (Jogo jogo) -> {
+            	if (andou && atacou) {
+            		andou = false;
+            		atacou = false;
+            		return new Ordem(Comando.ENCERRAR);
+            	}
+            	
+                int i, j;
+                
+                while (in.hasNext()) {
+                    switch (in.next()) {
+                        case "m":
+                            i = in.nextInt();
+                            j = in.nextInt();
+                            
+                        	if (!andou && jogo.mover(new Posicao(i, j))) {
+                        		andou = true;
+                        		return new Ordem(Comando.MOVER, i, j);
+                        	}
+                            break;
+                            
+                        case "a":
+                            i = in.nextInt();
+                            j = in.nextInt();
+                            
+                        	if (!atacou && jogo.atacar(new Posicao(i, j))) {
+                        		atacou = true;
+                        		return new Ordem(Comando.ATACAR, i, j);
+                        	}
+                        	break;
+                        	
+                        case "f":
+                        	andou = false;
+                        	atacou = false;
+                        	return new Ordem(Comando.ENCERRAR);
+                    }
                 }
-                return null;
-            }, InetAddress.getLoopbackAddress(), Servidor.PORTA_PADRAO);
-            in.close();
+                return new Ordem(Comando.ENCERRAR);
+                
+            };
+            Cliente c = new Cliente(cont, InetAddress.getLoopbackAddress(), Servidor.PORTA_PADRAO);
             
             try {
                 c.conectar();
