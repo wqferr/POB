54f4f6e355ca3f03a4e1943dede56f7bccb224d8
wqferr <wqferr@gmail.com>
2016-06-21 15:24:50 -0300

    Bugo tudo xeçus

--- RP2G/src/test/TestClient.java
+++ RP2G/src/test/TestClient.java
@@ -2,22 +2,38 @@ package test;
 
 import java.io.IOException;
 import java.net.InetAddress;
+import java.util.Scanner;
 
 import net.client.Cliente;
+import net.client.Comando;
+import net.server.Servidor;
+import core.Jogo;
 
 public class TestClient {
 
 	public static void main(String[] args) {
-		Cliente c = new Cliente(() -> { 
-			System.out.println("Acordado");
-			return null;
-		});
-		
-		try {
-			c.conectar(InetAddress.getLoopbackAddress());
-		} catch (IOException e) {
-			System.err.println(e);
-		}
+		boolean andou = false;
+		boolean atacou = false;
+		try (final Scanner in = new Scanner(System.in)) {
+            in.useDelimiter("[^\\s]");
+            Cliente c = new Cliente((Jogo j) -> {
+                if (!in.hasNext())
+                	return new Ordem(Comando.ENCERRAR);
+
+                switch (in.next()) {
+                case "m":
+                	break;
+                }
+                return null;
+            }, InetAddress.getLoopbackAddress(), Servidor.PORTA_PADRAO);
+            in.close();
+            
+            try {
+                c.conectar();
+            } catch (IOException e) {
+                System.err.println(e);
+            }
+        }
 	}
 
 }
