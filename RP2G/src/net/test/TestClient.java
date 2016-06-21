a21cf30d1939bf51ac6c6be3ea1ef2f8b23c59ce
wqferr <wqferr@gmail.com>
2016-06-21 11:55:43 -0300

    Consertar erro de compilação

---
+++ RP2G/src/net/test/TestClient.java
@@ -0,0 +1,23 @@
+package net.test;
+
+import java.io.IOException;
+import java.net.InetAddress;
+
+import net.client.Cliente;
+
+public class TestClient {
+
+	public static void main(String[] args) {
+		Cliente c = new Cliente(() -> { 
+			System.out.println("Acordado");
+			return null;
+		});
+		
+		try {
+			c.conectar(InetAddress.getLoopbackAddress());
+		} catch (IOException e) {
+			System.err.println(e);
+		}
+	}
+
+}
