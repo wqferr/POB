5982ecc47adcef720a3b722103756063ddb5fd4a
eduardoscsouza <eduardoscsouza@gmail.com>
2016-06-23 19:45:11 -0300

    Criar Main para o servidor

--- RP2G/src/mains/ServidorTextual.java
+++ RP2G/src/mains/ServidorTextual.java
@@ -1,6 +1,7 @@
 package mains;
 
 import java.io.IOException;
+import java.util.Scanner;
 
 import net.server.Servidor;
 import core.Jogo;
@@ -15,14 +16,14 @@ import core.mapa.Mapa;
 public class ServidorTextual {
 	public static void main(String[] args) {
 		DatabaseHandler dbHandle = new DatabaseHandler();
-		Mapa map = Mapa.get("Map1");
+		Scanner scan = new Scanner(System.in);
+		
+		System.out.println("Nome do Mapa:");
+		Mapa map = Mapa.get(scan.nextLine());
 		Jogo game = new Jogo(map);
 		
 		Servidor server = new Servidor(game);
-		try {
-            server.start();
-		} catch (IOException e) {
-			System.err.println(e);
-		}
+		try { server.start(); }
+		catch (IOException e){ System.err.println(e); }
 	}
 }
